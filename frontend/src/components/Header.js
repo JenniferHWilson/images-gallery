b6795b1ea7073a70cf69641a65d4b3c02b96da89
JenniferHWilson <98703625+JenniferHWilson@users.noreply.github.com>
2022-01-31 13:14:26 -0500

    Header creation

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar bg="light" variant="light">
+            <Navbar.Brand href="/">Images Gallery</Navbar.Brand> 
+        </Navbar>
+    )
+};
+
+export default Header;
+)
